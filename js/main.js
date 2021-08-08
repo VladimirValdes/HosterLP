28c4489a0d6376e3a5dc2e34aacc869ce76c031a
Vladimir Valdes <mendozahum20@gmail.com>
2021-08-08 15:22:49 -0600

    fix menu

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 const hamburger = document.querySelector("#hamburger");
 const navMenu = document.querySelector("#menu");
 const link = document.querySelectorAll("#menu-link");
+const header = document.querySelector("#header");
+
 
 
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("menu-container--active");
+    header.classList.toggle("header--active");
     hamburger.classList.toggle("active");
-    console.log('Hice click')
 });
 
 
 const closeMenu = () => {
     navMenu.classList.remove("menu-container--active");
+    header.classList.remove("header--active");
     hamburger.classList.remove("active");
+
 }
 
 link.forEach( l => l.addEventListener('click', closeMenu));
